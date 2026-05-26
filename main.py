e8ef57a6384261dbae4bc50616d05938d53e057c
L1F3atbhss <Nathan.chan3000@gmail.com>
2026-05-26 15:05:52 -0400

    Update main.py

--- main.py
+++ main.py
@@ -26,3 +26,42 @@ BUTTON_COLOR = (100, 100, 200)
 HOVER_COLOR = (150, 150, 255)
 BROWN = (128, 0, 0)
 
+# Player settings
+player_size = 150
+player_x = WIDTH // 2 - player_size // 2
+player_y = HEIGHT - player_size
+player_speed = 5
+player_velocity_y = 0
+jump_height = -15
+gravity = 1
+
+# Ground settings
+ground_level = HEIGHT - player_size
+
+# Function to create a gradient
+def draw_gradient(screen, color1, color2):
+    for i in range(HEIGHT):
+        r = color1[0] + (color2[0] - color1[0]) * i // HEIGHT
+        g = color1[1] + (color2[1] - color1[1]) * i // HEIGHT
+        b = color1[2] + (color2[2] - color1[2]) * i // HEIGHT
+        pygame.draw.line(screen, (r, g, b), (0, i), (WIDTH, i))
+
+# Title screen
+def show_title_screen():
+    running = True
+    while running:
+        screen.fill((0, 0, 0))
+        draw_gradient(screen, (139, 70, 20), (0, 0, 0))
+
+        # Draw title
+        font = pygame.font.Font(None, 100)
+        title_text = font.render("Fate: navy sea", True, WHITE)
+        text_rect = title_text.get_rect(center=(WIDTH // 2, HEIGHT // 3))
+        screen.blit(title_text, text_rect)
+
+        # Draw buttons
+        font = pygame.font.Font(None, 50)
+        start_text = font.render("Start", True, WHITE)
+        credits_text = font.render("Credits", True, WHITE)
+
+        
